a9cd688a0359a1def8798d9139ac00a61d255861
RBates <rbates@mjhassoc.com>
2019-04-16 22:22:56 -0400

    app handles framework and uses container to display page content based on route

---
+++ resources/js/components/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="page">
+        <div class="navbar">
+            <router-link :to="{ name: 'home' }"><img src="" /></router-link>
+
+            <div clas="navmenu">
+                <ul>
+                    <li><router-link :to="{ name: 'home' }">Home</router-link></li>
+                    <li><router-link :to="{ name: 'contactus' }">Contact Us</router-link></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="container">
+            <router-view></router-view>
+        </div>
+
+        <div class="footer">
+            <div>
+                <p>Copyright Richard Bates 2019&copy;</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
