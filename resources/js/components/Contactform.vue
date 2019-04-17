1f185c9075d0a7248023aae4d06a6a52df22d7b8
RBates <rbates@mjhassoc.com>
2019-04-16 23:09:18 -0400

    removed h1 tag

--- resources/js/components/Contactform.vue
+++ resources/js/components/Contactform.vue
@@ -1,13 +1,10 @@
 <template>
-    <div id="contactUs">
-        <h1>Contact US</h1>
-        <form id="contactUsForm">
-            <input id="contactUsName" type="text" placeholder="First and Last Name">
-            <input id="contactUsEmail" type="email" placeholder="example@laravel.com">
-            <textarea id="contactUsMessage" placeholder="Write your message here"></textarea>
-            <button id="contactUsSubmit" type="submit">Submit</button>
-        </form>
-    </div>
+    <form id="contactUsForm">
+        <input id="contactUsName" type="text" placeholder="First and Last Name">
+        <input id="contactUsEmail" type="email" placeholder="example@laravel.com">
+        <textarea id="contactUsMessage" placeholder="Write your message here"></textarea>
+        <button id="contactUsSubmit" type="submit">Submit</button>
+    </form>
 </template>
 <script>
     export default {}
