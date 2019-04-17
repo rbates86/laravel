2704e5fe1a917e12e7feaba2754c847fa734e1ac
RBates <rbates@mjhassoc.com>
2019-04-16 23:07:08 -0400

    created contact us form

---
+++ resources/js/components/Contactform.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="contactUs">
+        <h1>Contact US</h1>
+        <form id="contactUsForm">
+            <input id="contactUsName" type="text" placeholder="First and Last Name">
+            <input id="contactUsEmail" type="email" placeholder="example@laravel.com">
+            <textarea id="contactUsMessage" placeholder="Write your message here"></textarea>
+            <button id="contactUsSubmit" type="submit">Submit</button>
+        </form>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
