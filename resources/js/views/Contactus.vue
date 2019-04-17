e9ab0d604a9a837eeff76752044c5f47bf9b6c89
RBates <rbates@mjhassoc.com>
2019-04-16 23:07:29 -0400

    imported contactus form

--- resources/js/views/Contactus.vue
+++ resources/js/views/Contactus.vue
@@ -1,3 +1,14 @@
 <template>
-    <p>Contact Us</p>
+    <div>
+        <p>Contact Us</p>
+        <ContactForm></ContactForm>
+    </div>
 </template>
+<script>
+    import ContactForm from '../components/ContactForm'
+    export default {
+        components: {
+            ContactForm
+        }
+    }
+</script>
